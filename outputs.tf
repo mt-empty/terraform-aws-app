80471a46dd055c4727ce04bcbbe7db55f81c287c
MT <matt.git@protonmail.com>
2022-03-14 13:03:18 +1100

    implemented infra without any authorization mechanism

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,12 @@
 # defines what values I want to use/store
 
 output "instance_ip_addr" {
-    value = aws_instance.instance.private_ip
+    value = "${join(",", aws_instance.server[*].private_ip)}"
 }
 
-output "db_instance_addr" {
-    value = aws_db_instance.db_instance.address
+output "api_endpoint" {
+    value = "https://${aws_api_gateway_rest_api.ApiGatewayApi.name}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_stage.APIStageName.stage_name}/"
 }
+# output "db_instance_addr" {
+#     value = aws_db_instance.db_instance.address
+# }
