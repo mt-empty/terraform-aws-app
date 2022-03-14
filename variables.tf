a0842e7fdcae04ca12fbdeb968d0e49960569e61
MT <59728838+mt-empty@users.noreply.github.com>
2022-03-14 13:03:18 +1100

    implemented infra without any authorization mechanism

--- variables.tf
+++ variables.tf
@@ -10,12 +10,13 @@ variable "region" {
 variable "bucket_name" {
     description = "s3 bucket name"
     type = string
+    default = "fit5225-ass2-images-prod"
 }
-
-# variable "domain" {
-#   description = "value"
-
-# }
+variable "function_name" {
+  description = "object detection"
+  type = string
+  default = "object_detection"
+}
 
 variable "ami" {
     description = "Amazon machine image for ec2 instance"
@@ -29,15 +30,24 @@ variable "instance_type" {
     default = "t2.micro"
 }
 
+
+variable "db_name" {
+  description = "name for database"
+  type = string
+  default = "server_db"
+}
+
 variable "db_user" {
   description = "username for database"
   type = string
-  sensitive = true
+  # sensitive = true
+  default = "admin"
 }
 
 
 variable "db_pass" {
   description = "password for database"
   type = string
-  sensitive = true
+  # sensitive = true
+  default = "00abf0511a7f4cee5"
 }
