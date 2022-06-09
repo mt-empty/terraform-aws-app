a8b404545f605cacfff37705505c616d2e46180a
MT <59728838+mt-empty@users.noreply.github.com>
2022-06-09 18:21:27 +1000

    Integrated all frontend functionality with backend (#9)

--- frontend/src/components/remove.jsx
+++ frontend/src/components/remove.jsx
@@ -1,13 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { removeATag } from "../util.js";
 
 const Remove = () => {
-  const [inputTagString, setInputTagString] = useState(null);
-  const [inputURL, setInputURL] = useState(null);
+  const [inputTagRemoval, setInputTagRemoval] = useState({ tags: "", url: "" });
+  const [results, setResults] = useState("");
 
   const turnIntoAnArray = (text) => {
     const tags = text.split(",").map((item) => item.trim());
     return tags;
+  };
+
+  useEffect(() => {
+    remove();
+  }, [inputTagRemoval]);
+
+  const remove = async () => {
+    const tags = turnIntoAnArray(inputTagRemoval.tags);
+    const url = inputTagRemoval.url.trim();
+    if (tags.length > 0 && url.length > 0) {
+      const results = await removeATag(url, tags);
+      const data = JSON.parse(results.body);
+      console.log(data);
+      // if results property exists on data
+      if (data["Results"] != null) {
+        setResults(data["Results"]);
+      } else {
+        console.log(data);
+        setResults([`No image matched with tags: "${tags}" and url: "${url}"`]);
+      }
+    }
   };
 
   return (
@@ -22,29 +43,34 @@ const Remove = () => {
       <br />
       <input
         type="text"
-        name="imageTags"
+        name="imageTagRemoveTags"
+        id="imageTagRemoveTags"
         placeholder="dog, plane ..."
-        onChange={(event) => {
-          console.log(event.target.value);
-          setInputTagString(event.target.value);
-        }}
       />
       <input
         type="text"
-        name="imageURL"
+        name="imageTagRemoveURL"
+        id="imageTagRemoveURL"
         placeholder="image URL"
-        onChange={(event) => {
-          console.log(event.target.value);
-          setInputURL(event.target.value);
-        }}
       />
       <button
-        onClick={() =>
-          removeATag(turnIntoAnArray(inputTagString), inputURL.trim())
-        }
+        onClick={() => {
+          // console.log(document.getElementById("imageTagRemoveTags").value);
+          // console.log(document.getElementById("imageTagRemoveURL").value);
+          setInputTagRemoval({
+            tags: document.getElementById("imageTagRemoveTags").value,
+            url: document.getElementById("imageTagRemoveURL").value,
+          });
+        }}
       >
-        Remove
+        Submit
       </button>
+
+      <br />
+      <br />
+      <>Results:</>
+      <br />
+      <a>{results}</a>
     </div>
   );
 };
--- frontend/src/util.js
+++ frontend/src/util.js
@@ -9,91 +9,146 @@ const encodeToBase64 = async (file) => {
   });
 };
 
+/**
+ *
+ * @param {file} image
+ * @returns the results of the image upload
+ */
 const uploadImage = async (image) => {
   console.log(checkHealth());
-  const encodedImage = (await encodeToBase64(image)).split(",")[1];
-  const payload = JSON.stringify({
-    image_name: image.name,
-    content: encodedImage,
-  });
-  fetch(process.env.API_ENDPOINT.concat("upload"), {
-    method: "PUT",
-    body: payload,
-  })
-    .then((res) => res.json())
-    .then(console.log)
-    .catch((error) => console.error(error));
+  try {
+    const encodedImage = (await encodeToBase64(image)).split(",")[1];
+    const payload = JSON.stringify({
+      image_name: image.name,
+      content: encodedImage,
+    });
+    let res = await fetch(process.env.API_ENDPOINT.concat("upload"), {
+      method: "PUT",
+      body: payload,
+    });
+    res = await res.json();
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
+/**
+ *
+ * @param {file} image
+ * @returns the results of the image detection
+ */
 const detectImage = async (image) => {
   console.log(checkHealth());
-  const encodedImage = (await encodeToBase64(image)).split(",")[1];
 
-  const payload = JSON.stringify({
-    content: encodedImage,
-  });
-
-  fetch(process.env.API_ENDPOINT.concat("detect"), {
-    method: "POST",
-    body: payload,
-  })
-    .then((res) => res.json())
-    .then(console.log)
-    .catch((error) => console.error(error));
+  try {
+    const encodedImage = (await encodeToBase64(image)).split(",")[1];
+    const payload = JSON.stringify({
+      content: encodedImage,
+    });
+    let res = await fetch(process.env.API_ENDPOINT.concat("detect"), {
+      method: "POST",
+      body: payload,
+    });
+    res = await res.json();
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-const deleteAnImage = async (image) => {
+/**
+ *
+ * @param {string} url, the S3 url of the image
+ * @returns the results of the image deletion
+ */
+const deleteAnImage = async (url) => {
   console.log(checkHealth());
 
-  fetch(process.env.API_ENDPOINT.concat("delete"), {
-    method: "POST",
-    body: {
+  try {
+    const payload = JSON.stringify({
       url: url,
-    },
-  })
-    .then((res) => res.json())
-    .then(console.log)
-    .catch((error) => console.error(error));
+    });
+    let res = await fetch(process.env.API_ENDPOINT.concat("delete"), {
+      method: "POST",
+      body: payload,
+    });
+    res = await res.json();
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
+/**
+ *
+ * @param {string} url, the S3 url of the image
+ * @param {string[]} tags the tags associated with the image
+ * @returns the results of the image tag removal, which are S3 urls of the images with the given tags
+ */
 const removeATag = async (url, tags) => {
   console.log(checkHealth());
 
-  fetch(process.env.API_ENDPOINT.concat("remove"), {
-    method: "POST",
-    body: {
+  try {
+    const payload = JSON.stringify({
       url: url,
       tags: tags,
-    },
-  })
-    .then((res) => res.json())
-    .then(console.log)
-    .catch((error) => console.error(error));
+    });
+    let res = await fetch(process.env.API_ENDPOINT.concat("remove"), {
+      method: "POST",
+      body: payload,
+    });
+    res = await res.json();
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
+/**
+ *
+ * @param {string[]} tags the tags associated with the images
+ * @returns the results of the image search, which are S3 urls of the images with the given tags
+ */
 const searchForImage = async (tags) => {
   console.log(checkHealth());
 
-  fetch(process.env.API_ENDPOINT.concat("search"), {
-    method: "POST",
-    body: {
+  try {
+    const payload = JSON.stringify({
       tags: tags,
-    },
-  })
-    .then((res) => res.json())
-    .then(console.log)
-    .catch((error) => console.error(error));
+    });
+    let res = await fetch(process.env.API_ENDPOINT.concat("search"), {
+      method: "POST",
+      body: payload,
+    });
+    res = await res.json();
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
+/**
+ * Check if the server is up and running
+ * @returns {boolean} true if the server is up and running
+ * @throws {Error} if the server is down
+ */
 const checkHealth = async () => {
   try {
     const res = await fetch(process.env.API_ENDPOINT.concat("health"), {
       method: "GET",
     });
-    const message = await res.json();
-    return console.log(message);
+    console.log(res.ok);
+    return res.ok;
   } catch (error) {
-    return console.error(error);
+    console.error(error);
   }
 };
-export { uploadImage, detectImage, searchForImage, checkHealth, removeATag, deleteAnImage };
+export {
+  uploadImage,
+  detectImage,
+  searchForImage,
+  checkHealth,
+  removeATag,
+  deleteAnImage,
+};
