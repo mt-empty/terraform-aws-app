a0db9f616b651eaa399e94e2e2902040e49d6bc1
MT <matt.git@protonmail.com>
2022-06-09 18:16:10 +1000

    Integrated all frontend functionality with backend

--- frontend/src/components/detect.jsx
+++ frontend/src/components/detect.jsx
@@ -1,31 +1,84 @@
-import React, { useState} from "react";
-import { uploadImage} from '../util.js';
+import React, { useState, useEffect } from "react";
+import { detectImage } from "../util.js";
 
 const Detect = () => {
-    const [selectedImage, setSelectedImage] = useState(null);
-    return (
-      <div>
-        <h1>Detect an image</h1>
-        <a>Upload an image tag to get a list of similar images</a>
-        {selectedImage && (
-          <div>
-          <img width={"25rem"} src={URL.createObjectURL(selectedImage)} />
-          <br />
-          <button onClick={()=>setSelectedImage(null)}>Detect</button>
-          </div>
-        )}
-        <br />
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [submit, setSubmit] = useState(false);
+  const [results, setResults] = useState([]);
 
-        <br />
-        <input
-          type="file"
-          name="uploadedImage"
-          onChange={(event) => {
-            setSelectedImage(event.target.files[0]);
-          }}
-        />
-      </div>
-    );
+  useEffect(() => {
+    detect();
+  }, [submit]);
+
+  const detect = async () => {
+    if (selectedImage != null) {
+      const results = await detectImage(selectedImage);
+      const data = JSON.parse(results.body);
+      if (data["links"]?.length > 0) {
+        setResults(data["links"]);
+      } else if (data["links"] == 0) {
+        setResults(["No matching images found"]);
+      } else {
+        console.log(data);
+        setResults(["Error detecting the image"]);
+      }
+    }
+  };
+  return (
+    <div>
+      <h1>Detect an image</h1>
+      <a>Upload an image tag to get a list of similar images</a>
+      {selectedImage && (
+        <div>
+          <img width={"500rem"} src={URL.createObjectURL(selectedImage)} />
+          <br />
+          <a>{selectedImage.name}</a>
+          <br />
+          <a>
+            <button
+              onClick={() => {
+                setSelectedImage(null);
+                setResults([]);
+              }}
+            >
+              Remove
+            </button>
+            <button
+              onClick={() =>
+                setSubmit(document.getElementById("detectImage").files[0])
+              }
+            >
+              Submit
+            </button>
+          </a>
+        </div>
+      )}
+      <br />
+
+      <br />
+      <input
+        type="file"
+        name="detectImage"
+        id="detectImage"
+        onChange={(event) => {
+          setSelectedImage(event.target.files[0]);
+        }}
+      />
+      <br />
+      <br />
+      <>Results:</>
+      <br />
+      <ol type="1">
+        {/* separate string and put them in a list */}
+        {results.length > 0 &&
+          results.map((item) => (
+            <span key={item}>
+              <li>{item}</li>
+            </span>
+          ))}
+      </ol>
+    </div>
+  );
 };
 
 export default Detect;
