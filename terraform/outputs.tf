d467054c786b91ee22d190f332bceb20a884a22e
MT <59728838+mt-empty@users.noreply.github.com>
2022-06-20 21:59:32 +1000

    Added support for static site deployment on S3 (#11)
    * added support for static site deployment on S3
    * updated CI

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,14 @@
 # defines what values I want to use/store
 
 output "api_endpoint" {
-  description = "Ever green endpoint to the elastic beanstalk environment"
+  description = "Object detection endpoint of the API"
   value       = aws_apigatewayv2_api.ApiGatewayApi.api_endpoint
 }
 # output "db_instance_addr" {
 #     value = aws_db_instance.db_instance.address
 # }
+
+output "web_endpoint" {
+  description = "Web endpoint of s3 bucket"
+  value       = aws_s3_bucket.ReactAppBucket.website_endpoint
+}
